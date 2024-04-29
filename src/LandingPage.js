aac84c979ce863065a666cedf9d21b8f5e12415b
Rodney Bejabeng <Rodneyb2@illinois.edu>
2024-04-28 22:21:09 -0500

    Allow enter button to proceed

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -19,7 +19,8 @@ function LandingPage() {
     setIsValidEmail(validateEmail(emailInput));
   };
 
-  const handleTryBetaClick = () => {
+  const handleTryBetaClick = (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
     if (isValidEmail) {
       navigate('/credit');  // Adjust this to route to the appropriate page
     } else {
@@ -28,23 +29,25 @@ function LandingPage() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-teal-500 text-white">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-black to-blue-400 text-white">
       <h1 className="text-4xl font-bold">Welcome to Mintier (A Finance App)</h1>
       <p className="text-xl mt-4">Manage your finances with the insight of AI.</p>
-      <input
-        type="email"
-        value={email}
-        onChange={handleEmailChange}
-        placeholder="Enter your email"
-        className="mt-4 p-2 rounded text-black focus:outline-none focus:ring-2 focus:ring-yellow-500"
-      />
-      <button
-        onClick={handleTryBetaClick}
-        className={`mt-4 bg-yellow-500 hover:bg-yellow-600 text-Grey font-bold py-2 px-4 rounded ${!isValidEmail ? 'opacity-50 cursor-not-allowed' : ''}`}
-        disabled={!isValidEmail}
-      >
-        Try Beta!
-      </button>
+      <form onSubmit={handleTryBetaClick} className="w-full max-w-xs">
+        <input
+          type="email"
+          value={email}
+          onChange={handleEmailChange}
+          placeholder="Enter your email"
+          className="mt-4 p-2 w-full text-black rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
+        />
+        <button
+          type="submit"
+          className={`mt-4 bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 w-full rounded ${!isValidEmail ? 'opacity-50 cursor-not-allowed' : ''}`}
+          disabled={!isValidEmail}
+        >
+          Try Beta!
+        </button>
+      </form>
     </div>
   );
 }
