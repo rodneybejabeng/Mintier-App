ca75e3b7787ec67b2e00f9678cfcd3fe719a9453
Rodney Bejabeng <Rodneyb2@illinois.edu>
2024-05-05 22:02:41 -0500

    Adding Chart Boxes to components

---
+++ src/components/ChartBox/ChartBox.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import "./ChartBox.css"; // Make sure the path is correct
+import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts";
+
+const ChartBox = ({ color, icon, title, dataKey, number, percentage, chartData }) => {
+  return (
+    <div className="chartBox">
+      <div className="boxInfo">
+        <div className="title">
+          <img src={icon} alt="" />
+          <span>{title}</span>
+        </div>
+        <h1>{number}</h1>
+        <Link to="/" style={{ color: color }}>
+          View all
+        </Link>
+      </div>
+      <div className="chartInfo">
+        <div className="chart">
+          <ResponsiveContainer width="99%" height="100%">
+            <LineChart data={chartData}>
+              <Tooltip
+                contentStyle={{ background: "transparent", border: "none" }}
+                labelStyle={{ display: "none" }}
+                position={{ x: 10, y: 70 }}
+              />
+              <Line
+                type="monotone"
+                dataKey={dataKey}
+                stroke={color}
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="texts">
+          <span
+            className="percentage"
+            style={{ color: percentage < 0 ? "tomato" : "limegreen" }}
+          >
+            {percentage}%
+          </span>
+          <span className="duration">this month</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChartBox;
